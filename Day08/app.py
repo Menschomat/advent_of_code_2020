ca57914aaa70b7fea2ce2b4a19c51fef86a6c74e
Falco Weihrauch <falco@robaux.de>
2020-12-08 11:07:48 +0100

    added Day08

---
+++ Day08/app.py
@@ -0,0 +1,39 @@
+from copy import deepcopy
+
+def accumulator(in_data):
+    done = []
+    acc = 0
+    cursor = 0
+    while cursor not in done:
+        if cursor >= len(in_data):
+            return {'acc': acc, 'terminated': True, 'positions': done}
+        cur_task = in_data[cursor]
+        done.append(cursor)
+        if cur_task[0] == 'jmp':
+            cursor += cur_task[1]
+        else:
+            if cur_task[0] == 'acc':
+                acc += cur_task[1]
+            cursor += 1
+    return {'acc': acc, 'terminated': False, 'positions': done}
+
+def part_one(in_data):
+    return accumulator(in_data)
+
+def part_two(in_data):
+    def modify_in_data(in_data, idx):
+        n_in_data = deepcopy(in_data)
+        n_in_data[idx][0] = {"nop": "jmp", "jmp": "nop"}[n_in_data[idx][0]]
+        return n_in_data
+
+    # Do a run to find potential swap-candidates:
+    acc_jmp = [i for i in accumulator(in_data)['positions'] if in_data[i][0] == "nop" or in_data[i][0] == "jmp"]
+    for i in acc_jmp:
+        out = accumulator(modify_in_data(in_data, i))
+        if out['terminated'] is True:
+            return out
+
+with open('input.txt') as file:
+    in_data = [[line.split(" ")[0], int(line.split(" ")[1])]for line in file.read().split('\n')]
+
+print('Part1: ', part_one(in_data)['acc'], ' - Part2: ', part_two(in_data)['acc'])
